473885394be593f5d64e2a4115b9843942b77a15
Oliver Ullman <oliverullman@gmail.com>
2023-11-13 18:27:19 -0500

    export * example

--- examples/simple/src/utils/index.ts
+++ examples/simple/src/utils/index.ts
@@ -2,10 +2,11 @@ import { User } from './User';
 import { Article } from './Article';
 import { Category as ArticleCateogry } from './Category';
 import Group from './Group';
-import { Unused } from './Unused';
+// import { Unused } from './Unused';
 
 export { User };
-export { Unused };
+// export { Unused };
+export * from './Unused';
 export { Aggregated } from './Aggregated';
 export { Article, ArticleCateogry };
 export const TestValue = 'value';
